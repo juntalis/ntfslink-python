26c31327088134a14c738c61b5b290aa5124ea75
Charles Grunwald (Juntalis) <charles@grunwald.me>
2013-09-11 20:55:30 -0700

    fixes #4 - Cygwin Symbolic Link Support

--- ntfslink/cyglinks.py
+++ ntfslink/cyglinks.py
@@ -9,31 +9,86 @@ and/or modify it under the terms of the Do What The Fuck You Want
 To Public License, Version 2, as published by Sam Hocevar. See
 http://sam.zoy.org/wtfpl/COPYING for more details.
 """
-from common import *
+from .common import *
+
+__all__ = [ 'create', 'check', 'read', 'unlink' ]
 
 # Cygwin symbolic links start with:
-CYGLINK_SIGNATURE = [ 0x21, 0x3C, 0x73, 0x79, 0x6D, 0x6C, 0x69, 0x6E, 0x6B, 0x3E, 0xFF, 0xFE ]
+cyglink_tag = '!<symlink>'
 
-class CygLink(Structure):
+def utf8str(data):
+	databuf = data
+	if isinstance(data, unicode):
+		databuf = data.encode('utf8')
+	elif data[:2] == '\xff\xfe':
+		databuf = data.decode('utf16')
+	return databuf.rstrip('\0')
+
+def utf16str(data):
+	databuf = data
+	if isinstance(data, unicode):
+		databuf = data.decode('utf8').encode('utf16')
+	elif data[:2] != '\xff\xfe':
+		databuf = data.encode('utf16')
+	return databuf.rstrip('\0') + '\0\0'
+
+def verify_filepath(filepath):
+	""" Verify that a filepath has the proper attributes to be a cygwin symlink. """
+	return os.path.isfile(filepath) and \
+		(GetFileAttributesW(filepath) & FILE_ATTRIBUTE_SYSTEM) == FILE_ATTRIBUTE_SYSTEM
+
+def verify_data(data):
+	""" Verify that data contains a valid cygwin symlink """
+	databuf = data
+	if isinstance(data, file):
+		pos = data.tell()
+		data.seek(0)
+		databuf = data.read()
+		data.seek(pos)
+	return len(databuf) > 10 and databuf[:10] == cyglink_tag
+
+def create(srcpath, linkpath):
 	"""
-	CTypes implementation of:
+	Create a cygwin symbolic link at link_name pointing to srcpath.
 
-	typedef struct {
-		char tag[10]; 
-		unsigned short reserved1;
-		wchar_t target[ANYSIZE_ARRAY];
-	} cyglink;
+	See: os.symlink
 	"""
+	src = utf16str(srcpath)
+	with open(linkpath, 'wb') as f:
+		f.write(cyglink_tag)
+		f.write(src)
+	return SetFileAttributesW(srcpath, FILE_ATTRIBUTE_SYSTEM) != FALSE
 
-	#noinspection PyTypeChecker
-	_fields_ = [
-		("Reserved1", BYTE * 12), # '!<symlink>' followed by '\xff\xfe'
-		("PathBuffer", WCHAR * (MAX_PATH+1))
-	]
-	
-	def __init__(self, target=None):
-		super(CygLink, self).__init__()
-		if target is not None:
-			self.PathBuffer = target
-		for i, b in enumerate(CYGLINK_SIGNATURE):
-			self.Reserved1[i] = b
+def check(linkpath):
+	"""
+	Checks if filepath is a cygwin symbolic link.
+
+	See: os.path.islink
+	"""
+	if verify_filepath(linkpath):
+		with open(linkpath, 'rb') as f:
+			return verify_data(f)
+	return False
+
+def read(linkpath):
+	"""
+	Read the target of the cygwin symbolic link at filepath.
+
+	See: os.readlink
+	"""
+	if not verify_filepath(linkpath): raise InvalidLinkException(linkpath)
+	with open(linkpath, 'rb') as f: databuf = f.read()
+	if not verify_data(databuf): raise InvalidLinkException(linkpath)
+	return utf8str(databuf[10:])
+
+def unlink(linkpath):
+	"""
+	Remove the cygwin symbolic link at linkpath.
+
+	See: os.rmdir
+	"""
+	if not verify_filepath(linkpath): raise InvalidLinkException(linkpath)
+	if SetFileAttributesW(linkpath, FILE_ATTRIBUTE_NORMAL) == FALSE:
+		raise WinError()
+	os.unlink(linkpath)
+	return True, 0
